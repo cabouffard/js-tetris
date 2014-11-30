3a403f8a87fa0801fbf8fc224158a506bb2a7069
Charles-Andre Bouffard <cabouffard@gmail.com>
2014-11-30 00:40:11 -0500

    Added gameover tux for the gf

--- app/scripts/systems/gameState.js
+++ app/scripts/systems/gameState.js
@@ -2,16 +2,30 @@
 
 var Game = Game || {};
 
+function showGameOver() {
+  console.log ('game done!');
+  Game.context.font = 'italic 25pt Calibri';
+  Game.context.fillStyle = '#000';
+  Game.context.fillText('Game Over !', 15, 100);
+
+  var img = new Image();
+  img.onload = function() {
+    console.log ('done');
+    Game.context.drawImage(img, 15, 120, 175, 150);
+  };
+  img.src = 'images/game_over.png';
+
+  Game.$canvas.style.borderColor = 'red';
+
+  Game.paused = true;
+}
+
 Game.ECS.Systems.gameState = function gameState () {
   for (var x = 0; x < Game.Board.verticalSquares; x++) {
     if (Game.ECS.collisionBoard[0][x]) {
-      console.log ('game done!');
-      Game.context.font = 'italic 25pt Calibri';
-      Game.context.fillText('Game Over !', 15, 100);
-      Game.$canvas.style.borderColor = 'red';
-
-      Game.paused = true;
+      showGameOver();
     }
   }
 };
+
 
